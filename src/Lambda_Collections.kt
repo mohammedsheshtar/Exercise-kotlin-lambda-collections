ebec63ae715d8c4f9fae6f7c744eb5f220b6c44b
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-14 19:35:50 +0300

    finished part 2:  generated a name list and filtered it to display only names starting with 'A'

--- src/Lambda_Collections.kt
+++ src/Lambda_Collections.kt
@@ -4,4 +4,13 @@ fun main() {
     val nums = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     println("[*] ${nums.map {it * 2}}");
 
+    // part 2
+    println("\nPart 2");
+    val names = mutableListOf<String>("Alice", "Bob", "Amir", "Charlie", "Annie", "David");
+    println("[*] ${filterNames(names) { it.contains("A" )}}");
+
+}
+
+fun filterNames( names: MutableList<String>, filter: (String) -> Boolean ): List<String>{
+    return names.filter(filter);
 }
